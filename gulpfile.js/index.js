d5fcbaa3ebf6a9ff548cbacc7a9a56a08e380cb0
hsin-yu <tina2793778@gmail.com>
2022-02-19 18:51:55 +0800

    移除 CDN

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uncss = require('gulp-uncss');
 
 gulp.task('default', function () {
-    return gulp.src('./css/bootstrap.min.css')
+    return gulp.src('./css/*.css')
         .pipe(uncss({
             html: ['index.html', 'course_js.html', 'courses.html', 'signin.html']
         }))
